3737997cbb6abc3ff753726910210aee762b0846
Deni Dwi Arta <denidarta@gmail.com>
2026-03-20 19:40:11 +0700

    update create account dto

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateAccountDto {
 	@IsString()
 	name: string;
+
+	@Transform(({ value }) => Number(value))
+	@IsNumber()
+	accountNumber: number;
 }
